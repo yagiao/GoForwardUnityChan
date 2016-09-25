bfe8d930a8754dfaee6d528fef1bb3f9c49f24b5
青柳宏史 <foncllibenamgalkca@gmail.com>
2016-09-25 20:47:26 +0900

    レッスン７課題
    課題を提出のため

---
+++ Assets/CubeBGM.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBGM : MonoBehaviour {
+	private AudioSource sound01;
+
+
+	void Start () {
+		//AudioSourceコンポーネントを取得し、変数に格納
+		sound01 = GetComponent<AudioSource>();
+
+	}
+
+	void Update () {
+		
+		}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		
+		//障害物に衝突した場合
+		if (coll.gameObject.tag == "blocktag" || coll.gameObject.tag == "groundtag") {
+			sound01.PlayOneShot(sound01.clip);
+		}
+	}
+}
